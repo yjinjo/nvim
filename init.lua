85a2d6354580dba60c6a4715232c8ea38c59476b
Youngjin Jo <yjinjo@berkeley.edu>
2023-10-24 11:39:10 +0900

    [Update] Nvim-Tree File Explorer

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require("yjinjo.plugins-setup")  -- Should be at the top
 require("yjinjo.core.options")
 require("yjinjo.core.keymaps")
 require("yjinjo.core.colorscheme")
+require("yjinjo.plugins.comment")
+require("yjinjo.plugins.nvim-tree")
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,21 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["Comment.nvim"] = {
+    loaded = true,
+    path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/Comment.nvim",
+    url = "https://github.com/numToStr/Comment.nvim"
+  },
+  ReplaceWithRegister = {
+    loaded = true,
+    path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/ReplaceWithRegister",
+    url = "https://github.com/vim-scripts/ReplaceWithRegister"
+  },
+  ["nvim-tree.lua"] = {
+    loaded = true,
+    path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    url = "https://github.com/nvim-tree/nvim-tree.lua"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/packer.nvim",
@@ -84,10 +99,25 @@ _G.packer_plugins = {
     path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["vim-maximizer"] = {
+    loaded = true,
+    path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/vim-maximizer",
+    url = "https://github.com/szw/vim-maximizer"
+  },
   ["vim-nightfly-guicolors"] = {
     loaded = true,
     path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/vim-nightfly-guicolors",
     url = "https://github.com/bluz71/vim-nightfly-guicolors"
+  },
+  ["vim-surround"] = {
+    loaded = true,
+    path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/vim-surround",
+    url = "https://github.com/tpope/vim-surround"
+  },
+  ["vim-tmux-navigator"] = {
+    loaded = true,
+    path = "/Users/yjinjo/.local/share/nvim/site/pack/packer/start/vim-tmux-navigator",
+    url = "https://github.com/christoomey/vim-tmux-navigator"
   }
 }
 
