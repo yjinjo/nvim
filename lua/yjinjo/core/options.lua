df17acd870e17dbeaafd6fa3cbe2859f4984350d
Youngjin Jo <yjinjo@berkeley.edu>
2023-10-24 10:51:22 +0900

    Basic Options & Settings

---
+++ lua/yjinjo/core/options.lua
@@ -0,0 +1,52 @@
+local opt = vim.opt -- for conciseness
+
+-- https://youtu.be/3mCD4oBC4Xg?t=508
+opt.backup = false -- creates a backup file
+opt.cmdheight = 2 -- more space in the neovim command line
+opt.fileencoding = "utf-8" -- the encoding written to a file
+opt.hlsearch = true -- highlight all matches on previous search
+opt.smartindent = true -- make indenting smarter again
+opt.undofile = true -- enable persistent undo
+opt.updatetime = 300 -- faster completion (4000ms default)
+
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+
+-- tabs & indentation
+opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+opt.softtabstop = 4 -- tab replace with 4 spaces
+opt.swapfile = false
+
+-- line wrapping
+opt.wrap = false -- disable line wrapping
+opt.showmatch = true -- display matched parenthesis
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+opt.iskeyword:append("-") -- consider string-string as whole word
