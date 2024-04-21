4f1a7504ae22475f6b95715b9e9bd013df5a46a9
Youngjin Jo <yjinjo@berkeley.edu>
2024-04-21 14:56:30 +0900

    feat: setup linting

--- lua/yjinjo/plugins/lsp/mason.lua
+++ lua/yjinjo/plugins/lsp/mason.lua
@@ -1,62 +1,64 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
 
-		local mason_tool_installer = require("mason-tool-installer")
+    local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			-- https://github.com/williamboman/mason-lspconfig.nvim
-			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"ruff",
-				"tflint",
-				"lua_ls",
-				"jsonls",
-				"helm_ls",
-				"gopls",
-				"dockerls",
-				"rust_analyzer",
-				"taplo",
-				"yamlls",
-			},
-		})
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      -- https://github.com/williamboman/mason-lspconfig.nvim
+      ensure_installed = {
+        "tsserver",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "prismals",
+        "ruff",
+        "tflint",
+        "lua_ls",
+        "jsonls",
+        "helm_ls",
+        "gopls",
+        "dockerls",
+        "rust_analyzer",
+        "taplo",
+        "yamlls",
+      },
+    })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "ruff", -- python linter
+        "eslint_d", -- js linter
+      },
+    })
+  end,
 }
