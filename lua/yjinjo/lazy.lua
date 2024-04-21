79d60091dc807bd71ca5826cab08ad24aa541105
Youngjin Jo <yjinjo@berkeley.edu>
2024-04-21 13:56:07 +0900

    feat: setup LSP

--- lua/yjinjo/lazy.lua
+++ lua/yjinjo/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("yjinjo.plugins", {
+require("lazy").setup({ { import = "yjinjo.plugins" }, { import = "yjinjo.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
